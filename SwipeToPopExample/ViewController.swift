aa990208c47d77a7a921ece3dece6092700fb447
Yuya Hirayama <y@yura.me>
2016-02-24 16:03:26 +0900

    Initial Commit

---
+++ SwipeToPopExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwipeToPopExample
+//
+//  Created by smartapp on 2016/02/24.
+//  Copyright © 2016年 Yuya Hirayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
